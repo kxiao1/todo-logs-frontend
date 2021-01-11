246c3291ecbf710aa884bdfe62f1cab9ad612ea0
Karl Xiao <kzx@andrew.cmu.edu>
2021-01-11 02:29:32 -0500

    deploy attempt #5

--- genscript.sh
+++ genscript.sh
@@ -4,4 +4,4 @@ echo "export const modes = { daily: ${1}, weekly: ${2} };" > ${file}
 echo "export const serverAdd = \"${3}\";" >> ${file}
 echo "export const authAdd = \"${4}\";" >> ${file}
 echo "export const user = { username: \"${5}\", password: \"${6}\" };" >> ${file}
-< .src/constants.js
+< ./src/constants.js
